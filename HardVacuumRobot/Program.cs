2c15ae438b0e63f2e9ce8a495cfd4e4160ddc47e
Matthias Mailänder <matthias@mailaender.name>
2021-05-09 21:14:49 +0200

    Add a very basic bot that can connect.

---
+++ HardVacuumRobot/Program.cs
@@ -0,0 +1,81 @@
+﻿using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.EventArgs;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace OpenHV
+{
+    public class Program
+    {
+        public readonly EventId BotEventId = new EventId(42, "HV Bot");
+        
+        public DiscordClient Client { get; set; }
+
+        public static void Main(string[] args)
+        {
+            var prog = new Program();
+            prog.RunBotAsync().GetAwaiter().GetResult();
+        }
+
+        public async Task RunBotAsync()
+        {
+            var json = "";
+            using (var fileStream = File.OpenRead("config.json"))
+            using (var streamReader = new StreamReader(fileStream, new UTF8Encoding(false)))
+                json = await streamReader.ReadToEndAsync();
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+            var config = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+
+                AutoReconnect = true,
+                MinimumLogLevel = LogLevel.Debug
+            };
+
+            this.Client = new DiscordClient(config);
+
+            this.Client.Ready += this.ClientReady;
+            this.Client.GuildAvailable += this.GuildAvailable;
+            this.Client.ClientErrored += this.ClientError;
+
+            await this.Client.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task ClientReady(DiscordClient sender, ReadyEventArgs e)
+        {
+            sender.Logger.LogInformation(BotEventId, "Client is ready to process events.");
+
+            return Task.CompletedTask;
+        }
+
+        private Task GuildAvailable(DiscordClient sender, GuildCreateEventArgs e)
+        {
+            sender.Logger.LogInformation(BotEventId, $"Guild available: {e.Guild.Name}");
+
+            return Task.CompletedTask;
+        }
+
+        private Task ClientError(DiscordClient sender, ClientErrorEventArgs e)
+        {
+            sender.Logger.LogError(BotEventId, e.Exception, "Exception occured");
+
+            return Task.CompletedTask;
+        }
+    }
+
+    public struct ConfigJson
+    {
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+
+        [JsonProperty("prefix")]
+        public string CommandPrefix { get; private set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,27 @@
+This bot is based on [DSharpPlus](https://dsharpplus.github.io/). It's aim is to give a global multiplayer lobby experience.
+
+## Requirements
+The project was made using Visual Studio 2017, and targets .NET Core 3.1. You will need one of the following to open and build the projects:
+
+* [**Visual Studio 2019 Community**](https://www.visualstudio.com/thank-you-downloading-visual-studio/?sku=Community) with **.NET Core cross-platform development** installed.
+* [**Visual Studio Code**](https://code.visualstudio.com/download) with **C#** extension.
+
+You will also need to install [.NET Core 3.1 SDK](https://www.microsoft.com/net/download/core) in both cases.
+
+### Create the application
+1. Go to [Applications](https://discord.com/developers/applications) page on Discord Developer portal.
+2. Press the **New Application** button.
+3. **New Application** page will open. Enter the bot's name in the **name** field.
+4. When you're done, press the **create** button.
+5. When the app is created, jump to the **bot** section and press the **add bot** button.
+6. Once this is done, you will need to copy the **bot's token**. Under **app bot user**, there's a **token** field, press copy **the resulting value**.
+
+### Building the project and configuring the bot
+1. Open the project, select the startup bot, and compile it.
+2. Go to `HardVacuumBot` and edit `config.json` with the token.
+
+### Adding the bot to the server
+1. Go back to the app page, and copy the bot's **client ID**.
+2. Go to `https://discordapp.com/oauth2/authorize?client_id=the_app_id_here&scope=bot&permissions=0`.
+3. On the page, select **the server** (1), and press **authorize** (2).
+4. Verify that you are not a robot and you're done! You can now run the bot!
